a0266af54a6a9f3c8b7300dd14c28c7ef0876353
JRodriguez-Repo <97987743+JRodriguez-Repo@users.noreply.github.com>
2022-04-07 00:16:49 -0400

    enter

--- script.js
+++ script.js
@@ -13,22 +13,22 @@ searchBtn.onclick = () => {
     .then((data) => {
 
         console.log(data);
-        let output = `<h1>TV Shows</h1>`;
+        let output ='';
 
-        data.map((values)=>{
+        data.map((values) => {
 
             output += `
-            <div>
-
-             <img src="${values.show.image.medium}" alt="show image">
-            <ul class="list-group mb-3">
-              <li class="list-group-item">Name: ${values.show.name}</li>
-              <li class="list-group-item">Network: ${values.show.network.name}</li>
-              <li class="list-group-item">Summary: ${values.show.summary}</li>
-              <li class="list-group-item">Runtime: ${values.show.runtime}</li>
-            </ul> 
-            
-            </div>`
+                    
+                    <ul class="list-group">
+                    <img src= ${values.show.image.medium}  alt="show image">
+                    <li class="list-group-item">A fourth item</li>
+                    <li class="list-group-item">Show Name: ${values.show.name}</li>
+                    <li class="list-group-item">Premiered: ${values.show.premiered}</li>
+                    <li class="list-group-item">Runtime: ${values.show.runtime}</li>
+                    <li class="list-group-item">A fourth item</li>
+                    <li class="list-group-item">And a fifth one</li>
+                    </ul>
+             `    
 
         })
         
@@ -40,3 +40,24 @@ searchBtn.onclick = () => {
 
 
 
+/**
+ * 
+ *   <div class="container" id="output">
+            <div class="row">
+            <div class="col-lg-3">
+
+            <div class="col-md-3 col-lg-3 mx-auto border-rounded d-flex flex-row">
+              <div class="card" style="width: 18rem;">
+              <img src= ${values.show.image.medium} class="card-img-top" alt="">
+              <div class="card-body">
+              <h5 class="card-title">Show Name: ${values.show.name}</h5>
+              <p class="card-text">Summary: ${values.show.summary}</p>
+              <p class="card-text">Runtime: ${values.show.runtime}</p>
+              </div> 
+            </div></div>
+
+            </div>
+            </div>
+            </div>
+ * 
+ */
